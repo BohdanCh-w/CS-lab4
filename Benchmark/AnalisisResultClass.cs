9b359a1bd5610cfd8e408ae62a942ed9bcccae98
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-11-16 16:55:28 +0200

    Update Benchmark

--- Benchmark/AnalisisResultClass.cs
+++ Benchmark/AnalisisResultClass.cs
@@ -4,47 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Benchmark
-{
-    public class AnalisisResultClass : IDisposable
-    {//модифікатор public
+namespace Benchmark {
+    public class AnalisisResultClass : IDisposable {
         public uint analisisID { get; set; }
         public double temparature { get; set; }
         public string bloodTest { get; set; }
         public Dictionary<string, string> other { get; set; }
 
-        public AnalisisResultClass(uint _analisisID, double _temparature, string _bloodTest, Dictionary<string, string> _other)
-        {
+        public AnalisisResultClass(uint _analisisID, double _temparature, string _bloodTest, Dictionary<string, string> _other) {
             analisisID = _analisisID;
             temparature = _temparature;
             bloodTest = _bloodTest;
             other = _other;
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             int count = other?.Count ?? 0;
             var ret = $"id - {analisisID} : {temparature}*C, bloodTest - {bloodTest}, other - {count}. ";
-            if (count > 0)
-            {
-                foreach (var pair in other)
-                {
+            if (count > 0) {
+                foreach (var pair in other) {
                     ret += pair.ToString();
                 }
             }
             return ret;
         }
 
-        public void Dispose()
-        {
+        public void Dispose() {
             GC.SuppressFinalize(this);
         }
-        // public static string getParameter(this AnalisisResult res, string param) {
-        //     if ( res.other == null ) { return null; }
-        //     if (res.other.ContainsKey(param)) {
-        //         return res.other[param];
-        //     }
-        //     return null;
-        // }
     }
 }
--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -5,24 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Benchmark
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Benchmark {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Time for classes:" + CalculateClasses());
             Console.WriteLine("Time for structs:" + CalculateStructures());
         }
 
-        public static long CalculateClasses()
-        {
+        public static long CalculateClasses() {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            for (int i = 0; i < 100_000; i++)
-            {
-                using (AnalisisResultClass analisis = new AnalisisResultClass(1, 1.0, "", new Dictionary<string, string>()))
-                {
+            for (int i = 0; i < 100_000; i++) {
+                using (AnalisisResultClass analisis = new AnalisisResultClass(1, 1.0, "", new Dictionary<string, string>())) {
                     
                 }
             }
@@ -30,14 +24,11 @@ namespace Benchmark
             return stopwatch.ElapsedTicks;
         }
 
-        public static long CalculateStructures()
-        {
+        public static long CalculateStructures() {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            for (int i = 0; i < 100_000; i++)
-            {
-                using (AnalisisResultStruct analisis = new AnalisisResultStruct(1, 1.0, "", new Dictionary<string, string>()))
-                {
+            for (int i = 0; i < 100_000; i++) {
+                using (AnalisisResultStruct analisis = new AnalisisResultStruct(1, 1.0, "", new Dictionary<string, string>())) {
 
                 }
             }
